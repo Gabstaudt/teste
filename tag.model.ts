ca059558467b24014e990b546181e1fba90db184
Gabriella Staudt <161507433+Gabstaudt@users.noreply.github.com>
2024-10-07 06:58:40 -0300

    Create tag.model.ts

---
+++ tag.model.ts
@@ -0,0 +1,11 @@
+export class Tag {
+  id: number;
+  nome: string;
+  desc: string;
+  tipo: number;
+  max: number;
+  min: number;
+  status: number;
+  leituraInt?: number;
+  leituraBool?: boolean;
+}
